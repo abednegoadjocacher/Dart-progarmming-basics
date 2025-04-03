e6c1fd1a170299547be6075e89cc472426a8ff1a
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-04-03 16:22:20 +0000

    Practice variables in dart

---
+++ Dart-programming/variables.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  int age  = 200;
+  double price = 90;
+  String Name = 'Abednego';
+  
+bool Isvoting = true;
+
+  print('The integer value for age is : $age');
+  print('The dounle value for price is : $price');
+  print('The String value for Name is : $Name');
+  print('$Isvoting');
+}
