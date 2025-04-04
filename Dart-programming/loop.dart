f3ef4c0f3ce65f00f4549552d9edc9ee1f6634ff
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-04-04 22:42:17 +0000

    Loops in dart

---
+++ Dart-programming/loop.dart
@@ -0,0 +1,27 @@
+void main()
+{
+  // for loop in dart
+  // This is print just numbers
+  for(int i = 0; i <= 10; i++)
+  {
+    print(i);
+  }
+
+  // while loop in dart
+  //To also print ages from 20-56
+  int age = 20;
+  while(age <= 56)
+  {
+    print(age);
+    age++;
+  }
+
+  // do while loop
+  //To print a text to user
+  double number = 3.5;
+  do 
+  {
+    print('Thank you for your kindness');
+    number++;
+  } while (number <= 6.5 );
+}
