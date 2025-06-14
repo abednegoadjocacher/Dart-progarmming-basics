703512f2ad3b0ad58fbeb613e18ee750bc16a202
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-06-14 23:39:53 +0000

    Variablees in dart

--- Dart-programming/variables.dart
+++ Dart-programming/variables.dart
@@ -3,11 +3,12 @@ void main()
   int age  = 200;
   double price = 90;
   String Name = 'Abednego';
-  
+  dynamic number = 900;
 bool Isvoting = true;
 
   print('The integer value for age is : $age');
   print('The dounle value for price is : $price');
   print('The String value for Name is : $Name');
-  print('$Isvoting');
+  print('The boolean value of Isvoting is : $Isvoting');
+  print("The dynamic value of numberis : $number");
 }
