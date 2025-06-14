d447268ebc0a01d496f96c059cb1c5c44fa10798
Adjocacher Abednego <141531265+abednegoadjocacher@users.noreply.github.com>
2025-06-14 23:41:03 +0000

    Functions in dart

--- Dart-programming/helloworld.dart
+++ Dart-programming/helloworld.dart
@@ -1,6 +1,35 @@
+import 'dart:io';
 void main()
 {
+  //Reading input from the user
+  print('Enter number:');
+  var number = stdin.readLineSync();
+  var num2 =  int.parse(number!) * 2;
+  print(num2);
   
   print('hello Dart');
   print("Hi Annan");
+
+  // Function in Dart
+  myFunction(){
+    return "This is a function in Dart";
+  }
+ print(myFunction());
+ //Accepting User Input
+  String? name;
+  print("Enter your name: ");
+  name = stdin.readLineSync();
+  print("Hello, $name!");
+
+  // A function to add two numbers
+  addNumbers(){
+    var num1, num2;
+    print("Enter first number: ");
+    num1 = stdin.readLineSync();
+    print("Enter second number: ");
+    num2 = stdin.readLineSync();
+    var sum = int.parse(num1) + int.parse(num2);
+    print("The sum of $num1 and $num2 is: $sum");
+  }
+  addNumbers();
 }
